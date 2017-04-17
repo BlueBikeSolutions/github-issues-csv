6f6d85aa2be95f09ffbaa7430d529f426504eac0
Luke Benson <luke.benson@emergingstack.com>
2017-04-17 15:30:43 +1000

    trying different variables approach

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ENV_GITHUB_USER=${ghuser}
-export ENV_GITHUB_USER=${ghpass}
-export ENV_GITHUB_USER=${ghrepo}
-
-# Startup container(s)
-cat docker-compose.yml | envsubst | docker-compose -H $DOCKER_HOST_HOST up -d
