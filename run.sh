d94caa8d497795715281f0378d683de98d5627d3
Luke Benson <luke.benson@emergingstack.com>
2017-04-17 11:40:33 +1000

    fixing run command

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export ENV_GITHUB_USER=${ghpass}
 export ENV_GITHUB_USER=${ghrepo}
 
 # Startup container(s)
-cat docker-compose.yml | envsubst | docker-compose -H $DOCKER_HOST_HOST -f - up -d
+cat docker-compose.yml | envsubst | docker-compose -H $DOCKER_HOST_HOST up -d
